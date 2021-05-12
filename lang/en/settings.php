79566449566c7e83183015413ef414029b6bb00e
Bernard Condrau <bernard@condrau.com>
2021-05-12 14:21:45 +0700

    Add files via upload

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,7 +4,7 @@
  *
  * @license		GPL 3 (https://www.gnu.org/licenses/gpl-3.0.html)
  * @author		Bernard Condrau <bernard@condrau.com>
- * @version		2021-05-11, for Google Maps v3 API and DokuWiki Hogfather
+ * @version		2021-05-12, for Google Maps v3 API and DokuWiki Hogfather
  * @see			https://www.dokuwiki.org/plugin:googlemaps3
  * @see			https://www.dokuwiki.org/plugin:googlemaps
  * 
